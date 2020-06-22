836f6e353a2a94f5027e32f17e6e74b5fa83628b
kieva290 <kieva290@gmail.com>
2020-06-22 22:53:33 +0200

    Update RedditApplicationTests.java

--- src/test/java/com/bmw/reddit/RedditApplicationTests.java
+++ src/test/java/com/bmw/reddit/RedditApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RedditApplicationTests {
 
+    public RedditApplicationTests() {}
 // 	@Test
 // 	void contextLoads() {
 // 	}
